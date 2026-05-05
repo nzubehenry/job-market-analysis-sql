b21f8593c7cd46d945ad01341fd8276c96d40283
nzubehenry <nzubehenry06@gmail.com>
2026-05-05 18:58:27 +0100

    Initial commit: job market analysis project

---
+++ 02_sql/Most Optimal_skills.sql
@@ -0,0 +1,50 @@
+--OPTIMAL SKILLS (HIGH DEMAND + HIGH SALARY)
+
+WITH high_demand AS(
+SELECT
+	s.skill_id,
+	s.skills,
+	COUNT (b.skill_id) AS demand_count
+FROM fact_job_postings j INNER JOIN bridge_job_skills b
+ON j.job_id = b.job_id INNER JOIN dim_skills s
+ON b.skill_id = s.skill_id
+WHERE 
+	job_title_short like '%Analyst%'
+GROUP BY 
+	s.skill_id,s.skills
+
+), top_paying_skills AS (
+SELECT
+	s.skill_id,
+	ROUND(AVG (salary_annual), 2) avg_salary
+FROM fact_job_postings f 
+INNER JOIN bridge_job_skills b ON
+f.job_id = b.job_id 
+INNER JOIN dim_skills s ON
+b.skill_id = s.skill_id
+WHERE 
+	job_title_short like '%Analyst%' AND
+	salary_annual IS NOT NULL
+GROUP BY s.skill_id
+)
+
+SELECT 
+	h.skill_id,
+	h.skills,
+	demand_count,
+	avg_salary
+FROM 
+	high_demand h
+INNER JOIN top_paying_skills t ON
+h.skill_id = t.skill_id
+
+--WHERE demand_count > 10
+
+ORDER BY 
+avg_salary DESC,
+demand_count DESC
+
+-- Insight:
+-- The analysis reveals a subset of skills that are both highly demanded and associated with higher salaries.
+-- These "optimal skills" represent the most valuable areas for professionals to focus on, 
+-- as they offer both strong job opportunities and higher earning potential.
---
+++ 02_sql/Skills associated with high salary.sql
@@ -0,0 +1,25 @@
+--which skills are associated with higher salaries?
+
+SELECT 
+	skills,
+	AVG(salary_annual) AS average_salary
+
+FROM [dbo].[fact_job_postings] f
+JOIN [dbo].[bridge_job_skills] b
+ON f.job_id = b.job_id
+
+JOIN [dbo].[dim_skills] s
+ON b.skill_id = s.skill_id
+
+WHERE 
+	salary_annual IS NOT NULL
+
+GROUP BY
+	skills
+
+ORDER BY
+	average_salary DESC
+
+-- Insight:
+-- Certain technical skills are consistently associated with higher average salaries, indicating that employers place a premium on these capabilities.
+-- This highlights the importance of acquiring in-demand technical skills to maximize earning potential in the data field.
